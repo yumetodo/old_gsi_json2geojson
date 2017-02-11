364c67aa78f8306031b3c37ed2b579a75af9c7ab
yumetodo <yume-wikijp@live.jp>
2017-02-11 13:39:52 +0900

    プロジェクト ファイルを追加します。

---
+++ old_gsi_json2geojson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace old_gsi_json2geojson
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
