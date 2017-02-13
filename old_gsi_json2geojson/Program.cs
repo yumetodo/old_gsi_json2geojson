9ba2db92fd8f6763867b045347dfb14e45a1768c
yumetodo <yume-wikijp@live.jp>
2017-02-13 13:27:41 +0900

    add option class

--- old_gsi_json2geojson/Program.cs
+++ old_gsi_json2geojson/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
 
 namespace old_gsi_json2geojson
 {
+	class Options
+	{
+		[Option('b', HelpText = "Beautify output json.", DefaultValue = false)]
+		public bool Beautify { get; set; }
+		[Option('o', HelpText = "output path. Otherwise, Writeout to stdout.")]
+		public string Output { get; set; }
+		[Option('i', Required = true, HelpText = "input path")]
+		public string Input { get; set; }
+		[HelpOption(HelpText = "Display this help screen.")]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
 	class Program
 	{
 		static void Main(string[] args)
